82c90fb7f6a46823682aced2b8155566f3f33ca9
coeit <cbeiteneuer@gmail.com>
2021-02-16 18:08:23 +0100

    fix: MainPanel: add missing style 'content'

--- src/layout/MainPanel.jsx
+++ src/layout/MainPanel.jsx
@@ -47,6 +47,16 @@ const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
   },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+    width: '100%',
+  },
   contentShift: {
     transition: theme.transitions.create(['margin', 'width'], {
       easing: theme.transitions.easing.easeOut,
